22436b6c94aa4dcbed93bd8e0cb73f16646398b9
pavelyurlov <yurlov_pavel@mail.ru>
2025-06-04 23:45:14 +0300

    add phi-3 agent assistant

---
+++ exps/agent_runtime.py
@@ -0,0 +1,70 @@
+from dataclasses import dataclass
+import asyncio
+
+from autogen_core import (AgentId,
+                          MessageContext,
+                          RoutedAgent,
+                          message_handler,
+                          SingleThreadedAgentRuntime)
+from autogen_agentchat.agents import AssistantAgent
+from autogen_agentchat.messages import TextMessage
+from autogen_ext.models.openai import OpenAIChatCompletionClient
+
+from phi_inference import LLMConfig, LLM
+
+
+@dataclass
+class MyMessageType:
+    content: str
+
+
+class MyAgent(RoutedAgent):
+    def __init__(self) -> None:
+        super().__init__("MyAgent")
+
+    @message_handler
+    async def handle_my_message_type(self, message: MyMessageType, ctx: MessageContext) -> None:
+        print(f"{self.id.type} received message: {message.content}")
+
+
+class MyAssistant(RoutedAgent):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        model_client = OpenAIChatCompletionClient(model="gpt-4o")
+        self._delegate = AssistantAgent(name, model_client=model_client)
+
+    @message_handler
+    async def handle_my_message_type(self, message: MyMessageType, ctx: MessageContext) -> None:
+        print(f"{self.id.type} received message: {message.content}")
+        response = await self._delegate.on_messages(
+            [TextMessage(content=message.content, source="user")], ctx.cancellation_token
+        )
+        print(f"{self.id.type} responded: {response.chat_message}")
+
+
+class MyPhiAssistant(RoutedAgent):
+    def __init__(self, name: str) -> None:
+        super().__init__(name)
+        self.llm = LLM(LLMConfig())
+
+    @message_handler
+    async def handle_my_message_type(self, message: MyMessageType, ctx: MessageContext) -> None:
+        print(f"{self.id.type} received message: {message.content}")
+        response = self.llm(message.content)
+        print(f"{self.id.type} responded: {response}")
+
+
+async def main():
+    runtime = SingleThreadedAgentRuntime()
+    await MyAgent.register(runtime, "my_agent", lambda: MyAgent())
+    # await MyAssistant.register(runtime, "my_assistant", lambda: MyAssistant("my_assistant"))
+    await MyPhiAssistant.register(runtime, "my_assistant", lambda: MyPhiAssistant("my_assistant"))
+
+    runtime.start()
+    await runtime.send_message(MyMessageType("Hello, World!"), AgentId("my_agent", "default"))
+    await runtime.send_message(MyMessageType("Hello, World!"), AgentId("my_assistant", "default"))
+    await runtime.stop()
+
+
+if __name__ == '__main__':
+    asyncio.run(main())
---
+++ exps/phi_inference.py
@@ -0,0 +1,82 @@
+from __future__ import annotations
+
+import  torch
+from transformers import (AutoModelForCausalLM,
+                          AutoTokenizer)
+from transformers import pipeline
+
+from typing import TYPE_CHECKING
+if TYPE_CHECKING:
+    from phi_inference import LLMConfig
+
+
+class LLM:
+
+    def __init__(self, cfg: LLMConfig):
+        self.model_name = cfg.model_name
+        self.device = cfg.device
+        self.model = self.get_model()
+        self.tokenizer = self.get_tokenizer()
+        self.generator = self.get_generator(**cfg.pipeline_config)
+
+    def get_model(self, torch_dtype='auto', trust_remote_code=True):
+        model = AutoModelForCausalLM.from_pretrained(
+            self.model_name,
+            device_map=self.device,
+            torch_dtype=torch_dtype,
+            trust_remote_code=trust_remote_code
+        )
+        return model
+
+    def get_tokenizer(self):
+        tokenizer = AutoTokenizer.from_pretrained(self.model_name)
+        return tokenizer
+
+    def get_generator(self, **kwargs):
+        generator = pipeline(
+            model=self.model,
+            tokenizer=self.tokenizer,
+            **kwargs)
+        return generator
+
+    def encode(self, prompt, return_list=True):
+        input_ids = self.tokenizer(prompt, return_tensors='pt').input_ids
+        print(input_ids)
+        return input_ids[0] if return_list else input_ids
+
+    def decode(self, output_ids):
+        output = self.tokenizer.decode(output_ids)
+        print(output)
+        return output
+
+    def __call__(self, prompt, return_text=True):
+        messages = [
+            {'role': 'user', 'content': prompt}
+        ]
+        output = self.generator(messages)
+        return output[0]["generated_text"] if return_text else output
+
+
+class LLMConfig:
+    def __init__(self):
+        self.model_name = 'microsoft/Phi-3-mini-4k-instruct'
+        self.pipeline_config = {
+            'task': 'text-generation',
+            'return_full_text': False,
+            'max_new_tokens': 500,
+            'do_sample': False
+        }
+        self.device = 'cuda:0'
+
+
+def main(prompt="find x: x^2 - 2*x - 8 = 0"):
+    device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+    print(f'device: {device}, device count: {torch.cuda.device_count()}')
+    config = LLMConfig()
+    llm = LLM(config)
+    output = llm(prompt)
+    print(f'output: {output}')
+
+
+if __name__ == '__main__':
+    main()
